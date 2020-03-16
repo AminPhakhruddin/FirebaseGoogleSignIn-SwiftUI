e60f598042a2ab81aa8e0b781889a2f71d4144bc
aminPhakhruddin <amin.phakhruddin@gmail.com>
2020-03-16 17:25:42 -0500

    Initial Commit

---
+++ Firebase Google Sign In/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase Google Sign In
+//
+//  Created by Amin Phakhruddin on 3/16/20.
+//  Copyright © 2020 Amin Phakhruddin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
