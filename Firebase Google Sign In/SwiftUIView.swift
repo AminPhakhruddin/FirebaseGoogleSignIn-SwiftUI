dedf19c91684cb794da3401d080c0023480747e5
aminPhakhruddin <amin.phakhruddin@gmail.com>
2020-04-02 08:28:49 -0500

    First Commit of Firebase Google Sign In Tutorial application.

---
+++ Firebase Google Sign In/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Firebase Google Sign In
+//
+//  Created by Amin Phakhruddin on 3/17/20.
+//  Copyright © 2020 Amin Phakhruddin. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Firebase Google Sign In' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Firebase Google Sign In
+
+pod 'Firebase/Auth'
+pod 'GoogleSignIn'
+
+end
